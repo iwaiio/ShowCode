4aef59c81c2e3c55509be66fd61497343714aad0
iwai <iwaiworkio@gmail.com>
2024-06-20 00:27:46 +0300

    ver 0.0.1

---
+++ C++/NEW/_build/CMakeFiles/PROG1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PROG1.dir/prog.cpp.obj"
+  "CMakeFiles/PROG1.dir/prog.cpp.obj.d"
+  "PROG1.exe"
+  "PROG1.exe.manifest"
+  "PROG1.pdb"
+  "libPROG1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PROG1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- C++/NEW/prog.cpp
+++ C++/NEW/prog.cpp
@@ -1,1 +1,20 @@
-// hi
+#include <iostream>
+#include <string>
+#include <tuple>
+
+int main(void){
+    int k;
+    int i;
+
+    std::string Name;
+    std::tuple <int, char> TpArray[2];
+
+    i = 0;
+    k = 0 ;
+
+    std::cin >> std::get<char>(TpArray[k]);
+
+    std::cin;
+    
+    return 0;
+}
