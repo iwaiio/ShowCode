5747d55131c1d947c4b2b8c4df522a4f413e770c
iwai <iwaiworkio@gmail.com>
2024-09-19 01:50:00 +0300

    ver 0.1.5 :(((

--- C/T_Prog1/LIB/head.h
+++ C/T_Prog1/LIB/head.h
@@ -3,13 +3,24 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-//#include <gtk/gtk.h>
+#include <gtk/gtk.h>
 
-//#include <glib-2.0/glib/galloca.h>
-// #include <glib-2.0/glib/gi18n.h>
-// #include <gtk-4.0/gtk/gtk.h>
+extern uint16_t scr;
+extern uint8_t num1;
+extern uint8_t num2;
+extern long answ;
+extern uint8_t result;
+extern char task[16];
+extern char strScore[16];
 
-
+uint8_t file_overwrite(char *fileName, char *fileText);
+uint8_t file_read(char *fileName, char *buffer, uint16_t *score);
+uint8_t generate_nums(uint8_t *number1, uint8_t *number2, long *answer, char *buffer);
+uint8_t update_score_str_from_num(char *buffer, uint16_t *num);
+uint8_t update_score_num_from_str(char *buffer, uint16_t *num);
 
 #endif // __HEAD_H__
