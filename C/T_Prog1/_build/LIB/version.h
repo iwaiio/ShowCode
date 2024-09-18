f9893bf1aaf89d830467fc95137935481ea3755b
iwai <iwaiworkio@gmail.com>
2024-09-19 02:01:25 +0300

    ver 0.1.6 trying

--- C/T_Prog1/_build/LIB/version.h
+++
@@ -1,5 +0,0 @@
-#define PROJECT_VERSION_MAJOR 0
-#define PROJECT_VERSION_MINOR 1
-#define PROJECT_VERSION_PATCH 6
-#define PROJECT_VERSION "0.1.6"
-// файл version.h генерируется в _build/LIB/version.h
